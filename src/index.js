043ce1ee5a694e4dfe963047ed684466ece3a4d6
hyunji jhang <syub98774@naver.com>
2024-01-07 15:18:20 +0900

    테스트

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h2>용감한 원정대 메인페이지입니다.</h2>
+    <h2>용감한 원정대</h2>
   </React.StrictMode>
 );
